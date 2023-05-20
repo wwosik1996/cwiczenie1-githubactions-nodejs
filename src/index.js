301aa29a4e039eabe2e2003ba152970ded29ea2c
Ubuntu <wiktoriawosik@github.igszqeztw1eexcxty1la1kk4ig.bx.internal.cloudapp.net>
2023-05-20 09:42:29 +0000

    dodane

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ const app = require("./app");
 const port = process.env.PORT || 3000;
 
 app.listen(port, () =>
-	console.log('Aplikacja nasłuchuje na porcie 3000');
+	console.log('Aplikacja nasłuchuje na porcie 3000'),
 );
